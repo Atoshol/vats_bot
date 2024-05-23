640d4d9dc55e6b89c490f9a9f59ec02c69515655
romamyhasuik <migasuiikr@gmail.com>
2024-05-23 13:28:50 +0300

    add user_settings model

--- db/models/user_settings.py
+++ db/models/user_settings.py
@@ -1,4 +1,4 @@
-from sqlalchemy import (Column, String, select, BigInteger, DateTime, func)
+from sqlalchemy import (Column, String, select, BigInteger, DateTime, func, Boolean, Integer)
 from db.crud import AsyncCRUD
 from db.engine import Base
 from decorators.db_session import db_session
@@ -8,6 +8,25 @@ class UserSettings(Base):
     __tablename__ = 'user_settings'
 
     id = Column(BigInteger, primary_key=True, index=True)
+
+    market_cap_max = Column(BigInteger, nullable=True, default=500000)
+    market_cap_min = Column(BigInteger, nullable=True, default=50000)
+    volume_5_minute_min = Column(BigInteger, nullable=True, default=15000)
+    volume_1_hour_min = Column(BigInteger, nullable=True)
+
+    liquidity_min = Column(BigInteger, nullable=True, default=20000)
+    liquidity_max = Column(BigInteger, nullable=True, default=200000)
+
+    price_change_5_minute_min = Column(Integer, nullable=True, default=100)
+    price_change_1_hour_min = Column(Integer, nullable=True, default=100)
+
+    transaction_count_5_minute_min = Column(BigInteger, nullable=True, default=10)
+    transaction_count_1_hour_min = Column(BigInteger, nullable=True, default=10)
+
+    holders_min = Column(BigInteger, nullable=True)
+    renounced = Column(Boolean, nullable=True, default=True)
+
+    include_old_pairs = Column(DateTime, nullable=True, default=func.now())
 
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
